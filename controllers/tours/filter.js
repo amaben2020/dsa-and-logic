95c26a807ccba530016a41f774dbb127656d2125
algomachine007 <amaben202phantom@gmail.com>
2023-06-23 19:54:25 +0100

    testing out req query

---
+++ controllers/tours/filter.js
@@ -0,0 +1,22 @@
+router.get("/tasks", auth, async (req, res) => {
+  const match = {};
+  if (req.query.completed) {
+    match.completed = req.query.completed === "true";
+  }
+  try {
+    await req.user
+      .populate({
+        path: "tasks",
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip),
+          sort,
+        },
+      })
+      .execPopulate();
+    res.send(req.user.tasks);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
--- routes/wiki.js
+++ routes/wiki.js
@@ -3,7 +3,19 @@ import express from "express";
 const wikiRouter = express.Router();
 
 wikiRouter.get("/", function (req, res) {
-  res.send("Wiki home page");
+  console.log("query", req.query);
+  const matches = {};
+  const { name, sex } = req.query;
+  if (name || sex) {
+    matches.info = {
+      name: name,
+      sex,
+    };
+  }
+  const information = matches["info"];
+  res.status(200).json({
+    info: information,
+  });
 });
 
 export default wikiRouter;
